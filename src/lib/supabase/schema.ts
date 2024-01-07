03f923b165ced4de21a1d8baa7da150a7bfcecb2
Elham-EN <ilhamrezaie@gmail.com>
2024-01-08 03:08:45 +1100

    created stripe subscription relation of tables and migrated to supa's postgres

--- src/lib/supabase/schema.ts
+++ src/lib/supabase/schema.ts
@@ -2,9 +2,20 @@
  * Create tables for the postgres database
  */
 
-import { pgTable, text, timestamp, uuid } from "drizzle-orm/pg-core";
+import {
+  boolean,
+  foreignKey,
+  integer,
+  jsonb,
+  pgTable,
+  text,
+  timestamp,
+  uuid,
+} from "drizzle-orm/pg-core";
+import { prices, subscriptionStatus, users } from "../../../migrations/schema";
+import { sql } from "drizzle-orm";
 
-export const workspace = pgTable("workspaces", {
+export const workspaces = pgTable("workspaces", {
   id: uuid("id").defaultRandom().primaryKey().notNull(),
   createdAt: timestamp("created_at", {
     withTimezone: true,
@@ -17,4 +28,92 @@ export const workspace = pgTable("workspaces", {
   inTrash: text("in_trash"),
   logo: text("logo"),
   bannerUrl: text("banner_url"),
+});
+
+export const folders = pgTable("folders", {
+  id: uuid("id").defaultRandom().primaryKey().notNull(),
+  createdAt: timestamp("created_at", {
+    withTimezone: true,
+    mode: "string",
+  }),
+  title: text("title").notNull(),
+  iconId: text("icon_id").notNull(),
+  data: text("data"),
+  inTrash: text("in_trash"),
+  logo: text("logo"),
+  bannerUrl: text("banner_url"),
+  // specify that when a row is deleted from the parent table, all rows
+  // in the child table that reference the deleted row should also be deleted
+  workspaceId: uuid("workspace_id").references(() => workspaces.id, {
+    onDelete: "cascade",
+  }),
+});
+
+export const files = pgTable("files", {
+  id: uuid("id").defaultRandom().primaryKey().notNull(),
+  createdAt: timestamp("created_at", {
+    withTimezone: true,
+    mode: "string",
+  }),
+  title: text("title").notNull(),
+  iconId: text("icon_id").notNull(),
+  data: text("data"),
+  inTrash: text("in_trash"),
+  logo: text("logo"),
+  bannerUrl: text("banner_url"),
+  // specify that when a row is deleted from the parent table, all rows
+  // in the child table that reference the deleted row should also be deleted
+  workspaceId: uuid("workspace_id").references(() => workspaces.id, {
+    onDelete: "cascade",
+  }),
+  folderId: uuid("folder_id").references(() => folders.id, {
+    onDelete: "cascade",
+  }),
+});
+
+export const subscriptions = pgTable("subscriptions", {
+  id: text("id").primaryKey().notNull(),
+  userId: uuid("user_id")
+    .notNull()
+    .references(() => users.id),
+  status: subscriptionStatus("status"),
+  metadata: jsonb("metadata"),
+  priceId: text("price_id").references(() => prices.id),
+  quantity: integer("quantity"),
+  cancelAtPeriodEnd: boolean("cancel_at_period_end"),
+  created: timestamp("created", { withTimezone: true, mode: "string" })
+    .default(sql`now()`)
+    .notNull(),
+  currentPeriodStart: timestamp("current_period_start", {
+    withTimezone: true,
+    mode: "string",
+  })
+    .default(sql`now()`)
+    .notNull(),
+  currentPeriodEnd: timestamp("current_period_end", {
+    withTimezone: true,
+    mode: "string",
+  })
+    .default(sql`now()`)
+    .notNull(),
+  endedAt: timestamp("ended_at", {
+    withTimezone: true,
+    mode: "string",
+  }).default(sql`now()`),
+  cancelAt: timestamp("cancel_at", {
+    withTimezone: true,
+    mode: "string",
+  }).default(sql`now()`),
+  canceledAt: timestamp("canceled_at", {
+    withTimezone: true,
+    mode: "string",
+  }).default(sql`now()`),
+  trialStart: timestamp("trial_start", {
+    withTimezone: true,
+    mode: "string",
+  }).default(sql`now()`),
+  trialEnd: timestamp("trial_end", {
+    withTimezone: true,
+    mode: "string",
+  }).default(sql`now()`),
 });
