d8ebaf791f071693f58e275273b426828e13b174
Elham-EN <ilhamrezaie@gmail.com>
2024-02-11 16:34:34 +1100

    solved hydration issue

--- src/components/global/emoji-picker.tsx
+++ src/components/global/emoji-picker.tsx
@@ -1,18 +1,18 @@
-"use client"; //* Client Component
-import { useRouter } from "next/navigation";
+"use client";
+// import { useRouter } from "next/navigation";
 import dynamic from "next/dynamic";
-import React, { ReactElement } from "react";
+import React, { ReactElement, ReactNode } from "react";
 import { EmojiClickData, Theme } from "emoji-picker-react";
 import { Popover, PopoverContent, PopoverTrigger } from "../ui/popover";
 
 interface EmojiPickerProps {
-  children: React.ReactNode;
+  children: ReactNode;
   getValue?: (emoji: string) => void;
 }
 
 function EmojiPicker({ children, getValue }: EmojiPickerProps): ReactElement {
-  const route = useRouter();
-  const Picker = dynamic(() => import("emoji-picker-react"));
+  //const route = useRouter();
+  const Picker = dynamic(() => import("emoji-picker-react"), { ssr: false });
   // Callback function that is called when an emoji is clicked.
   // The callback receives the event and the emoji data
   const onClick = (selectedEmoji: EmojiClickData) => {
@@ -21,7 +21,8 @@ function EmojiPicker({ children, getValue }: EmojiPickerProps): ReactElement {
   return (
     <div className="flex items-center">
       <Popover>
-        <PopoverTrigger className="cursor-pointer">{children}</PopoverTrigger>
+        {/* If it's a string, wrap it into span element */}
+        {typeof children === "string" ? <span>{children}</span> : children}
         <PopoverContent className="p-0 border-none">
           <Picker onEmojiClick={onClick} />
         </PopoverContent>
